eb4836e99837676775569d0a2dc00d02f14425ca
Carlos <c.salas.marin@gmail.com>
2016-06-08 21:37:54 -0400

    ssh:doctor funciona  capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :stage, :production
 set :rails_env, :production
 
-server 'ec2-184-72-95-8.compute-1.amazonaws.com', user: 'username', roles: %w{web app db}, port: 443,primary: true
+server 'ec2-184-72-95-8.compute-1.amazonaws.com', user: 'ubuntu', roles: %w{web app db}, port: 443,primary: true
 
 # server-based syntax
 # ======================
